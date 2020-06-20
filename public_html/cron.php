69cac267be89f142c81f95cf8e7de532ac7da5ca
Clerick <13_spirit@mail.ru>
2020-06-20 20:20:53 +0100

    put telegram ids to env file

--- public_html/cron.php
+++ public_html/cron.php
@@ -17,15 +17,12 @@ $dotenv = \Dotenv\Dotenv::create($env_path);
 $dotenv->load();
 $bot_api_key = getenv('BOT_TOKEN');
 $bot_username = getenv('BOT_USERNAME');
+$chat_ids = array_map('trim', explode(',', getenv('TG_ACCOUNT_IDS')));
 $commands_path = dirname(__DIR__) . "/app/Models/TGBot/Commands/";
 $dbConfig = new DatabaseConfiguration();
 $db = new SQLDB($dbConfig);
 $telegram = new Telegram($bot_api_key, $bot_username);
 $siteClassNames = SiteUtil::getSiteClassNames();
-$chat_ids = [
-    357496770,
-    471334467
-];
 $telegram->enableLimiter();
 
 foreach ($siteClassNames as $siteClassName) {
